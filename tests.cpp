0333bea703bfe462ba3438fbaa4878dd18697900
Your Name <shubhamgupta190496@gmail.com>
2016-01-04 17:32:25 +0530

    Test cases gen

---
+++ tests.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define LL long long int
+
+using namespace std;
+int main()
+{
+    int a,b,c,t=100,i;
+    cout<<t<<endl;
+    for(i=0;i<t;i++)
+    {
+        a=(rand()%1000000+1);
+        b=(rand()%1000000+1);
+        c=(rand()%1000000+1);
+        printf("%d %d %d\n",a,b,c);
+    
+    
+    }
+    return 0;
+} 
